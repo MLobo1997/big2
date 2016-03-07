ca07b52f1fa3ad5d6715cae883f6f056a29da9cf
mlobo1997 <miguelobo1997@gmail.com>
2016-03-07 12:53:06 +0000

    codigo pos aula

--- src/cartas.c
+++ src/cartas.c
@@ -21,12 +21,22 @@ Ordem das cartas
 */
 #define VALORES		"3456789TJQKA2"
 
+#define TAM_MAX_ESTADO      1024
+
+#define FORMATO             "%llx_%llx_%llx_%llx"
+
+#define FORMATO2             "q=%llx_%llx_%llx_%llx"
+
+void shuffle (long long unsigned int *fourhands);
+
+char *da_estado(long long unsigned int *mao);
+
 /**
 	Estado inicial com todas as 52 cartas do baralho
 	Cada carta é representada por um bit que está
 	a 1 caso ela pertença à mão ou 0 caso contrário
 */
-const long long int ESTADO_INICIAL = 0xfffffffffffff;
+/*const long long int ESTADO_INICIAL = 0xfffffffffffff; */
 
 
 /** \brief Devolve o índice da carta
@@ -84,11 +94,16 @@ int carta_existe(long long int ESTADO, int naipe, int valor) {
 @param naipe	O naipe da carta (inteiro entre 0 e 3)
 @param valor	O valor da carta (inteiro entre 0 e 12)
 */
-void imprime_carta(char *path, int x, int y, long long int ESTADO, int naipe, int valor) {
+void imprime_carta(char *path, int x, int y, long long unsigned int *fourhands, int m, int naipe, int valor) {
 	char *suit = NAIPES;
 	char *rank = VALORES;
 	char script[10240];
-	sprintf(script, "%s?q=%lld", SCRIPT, rem_carta(ESTADO, naipe, valor));
+	long long unsigned int tfourhands[4] = {fourhands [0] , fourhands [1] , fourhands [2] , fourhands [3]};
+
+	rem_carta(tfourhands[m], naipe, valor);
+
+	sprintf(script, "%s?q=%s", SCRIPT, da_estado(tfourhands));
+	/*sprintf(script, "%s?q=%lld", SCRIPT, rem_carta(ESTADO, naipe, valor));*/
 	printf("<a xlink:href = \"%s\"><image x = \"%d\" y = \"%d\" height = \"110\" width = \"80\" xlink:href = \"%s/%c%c.svg\" /></a>\n", script, x, y, path, rank[valor], suit[naipe]);
 }
 
@@ -98,20 +113,24 @@ Esta função está a imprimir o estado em quatro colunas: uma para cada naipe
 @param path	o URL correspondente à pasta que contém todas as cartas
 @param ESTADO	O estado atual
 */
-void imprime(char *path, long long int ESTADO) {
-	int n, v;
+void imprime(char *path, long long unsigned int *fourhands) {
+	int n, v, m;
 	int x, y;
 
 	printf("<svg height = \"800\" width = \"800\">\n");
 	printf("<rect x = \"0\" y = \"0\" height = \"800\" width = \"800\" style = \"fill:#007700\"/>\n");
 
-	for(y = 10, n = 0; n < 4; n++, y += 120) {
-		for(x = 10, v = 0; v < 13; v++)
-			if(carta_existe(ESTADO, n, v)) {
-				x += 20;
-				imprime_carta(path, x, y, ESTADO, n, v);
+	for(y = 10, m = 0; m < 4; m++, y += 120){
+
+		for(x = 10, v = 0; v < 13; v++){
+			for(n = 0; n < 4; n++){
+				if(carta_existe(fourhands[m], n, v)) {
+					x += 20;
+					imprime_carta(path, x, y, fourhands, m , n, v);
+					}
+				}
 			}
-	}
+		}
 	printf("</svg>\n");
 }
 
@@ -123,21 +142,26 @@ Cada carta corresponde a um bit que está a 1 se essa carta está no conjunto e 
 Caso não seja passado nada à cgi-bin, ela assume que todas as cartas estão presentes.
 @param query A query que é passada à cgi-bin
  */
-void parse(char *query) {
-	long long int ESTADO;
-	if(sscanf(query, "q=%lld", &ESTADO) == 1) {
-		imprime(BARALHO, ESTADO);
+void parse(char *query , long long unsigned int *fourhands) {
+
+	long long int x;
+
+	if(sscanf(query, "q=%lld", &x) == 1) {
+		imprime(BARALHO, fourhands);
 	} else {
-		imprime(BARALHO, ESTADO_INICIAL);
+
+		shuffle (fourhands);
+
+		imprime(BARALHO, fourhands);
 	}
 }
 
 
-shuffle (long long unsigned int fourhands[]){
+void shuffle (long long unsigned int *fourhands){
 
 	int naipe, valor, randN;
 
-	int maos [4] = {0};
+	int countmaos [4] = {0};
 
 	srandom (time (NULL));
 
@@ -147,10 +171,10 @@ shuffle (long long unsigned int fourhands[]){
 
 			randN = random()%4;
 
-			while (maos[randN] == 13)
+			while (countmaos[randN] == 13)
 				randN = random() % 4; 
 
-			maos[randN]++;
+			countmaos[randN]++;
 
 			fourhands [randN] = add_carta(fourhands[randN] , naipe , valor);
 
@@ -160,35 +184,74 @@ shuffle (long long unsigned int fourhands[]){
 
 }
 
+
+int le_estado(char *estado, long long unsigned int *mao) {
+    if(estado == NULL || strlen(estado) == 0)
+        return 0;
+    return sscanf(estado, FORMATO2, mao, mao + 1, mao + 2, mao + 3);
+}
+
+char *da_estado(long long unsigned int *mao) {
+    char *estado = NULL;
+    estado = (char *) malloc (TAM_MAX_ESTADO);
+    sprintf(estado, FORMATO, mao[0], mao[1], mao[2], mao[3]);
+    return estado;
+}
+
+/*int main() {
+    long long unsigned int mao[4] = {0};
+    char *estado;
+    int i;
+
+    printf("Content-Type: text/html; charset=utf-8\n\n");
+
+    le_estado(getenv("QUERY_STRING"), mao);
+
+    estado = da_estado(mao);
+
+    printf("<p>estado = %s</p>\n", estado);
+    for(i = 0; i < 4; i++) {
+        printf("<p>mao[%d] = %llu</p>\n", i, mao[i]);
+    }
+*/
+
+
 /** \brief Função principal
 
 Função principal do programa que imprime os cabeçalhos necessários e depois disso invoca
 a função que vai imprimir o código html para desenhar as cartas
  */
 int main() {
-/*
- * Cabeçalhos necessários numa CGI
+
+	long long unsigned int fourhands [4] = {0};
+	char *estado;
+	int i;
+	
+	/** Cabeçalhos necessários numa CGI
  */
 	printf("Content-Type: text/html; charset=utf-8\n\n");
 	printf("<header><title>Exemplo</title></header>\n");
 	printf("<body>\n");
 
 	printf("<h1>Exemplo de utilização</h1>\n");
+
+    le_estado(getenv("QUERY_STRING"), fourhands);
+
+   estado = da_estado(fourhands);
+
+    printf("<p>estado = %s</p>\n", estado);
+    for(i = 0; i < 4; i++) {
+        printf("<p>mao[%d] = %llu</p>\n", i, fourhands[i]);
+    	}
+
 	
 
 /*
  * Ler os valores passados à cgi que estão na variável ambiente e passá-los ao programa
  */
-	parse(getenv("QUERY_STRING"));
+	parse(getenv("QUERY_STRING") , fourhands);
 
 	printf("</body>\n");
-
-	long long unsigned int fourhands [4] = {0}; 
-
-	shuffle(fourhands);
-
-	imprime(BARALHO , fourhands [0]);
-
 
 	return 0;
 }
